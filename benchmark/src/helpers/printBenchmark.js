3f88edb6e278399ea49c1bd6ecd8264900508646
karak <karak97@gmail.com>
2018-03-22 18:01:29 +0900

    Change benchmark results to mean and median.

--- benchmark/src/helpers/printBenchmark.js
+++ benchmark/src/helpers/printBenchmark.js
@@ -2,11 +2,30 @@
 
 var path = require("path");
 var Benchmark = require("benchmark");
+var median = require('median');
 var writeFileAsync = require("./writeFileAsync");
 
 /** Timeout duration */
 var TIMEOUT_MILLISECONDS = 1000;
 
+/**
+ * @typedef Stats
+ * @type {object}
+ * @property {number} deviation - The sample standard deviation.
+ * @property {number} mean - The sample arithmetic mean (secs).
+ * @property {number} moe - The margin of error.
+ * @property {number} rme - The relative margin of error (expressed as a percentage of the mean).
+ * @property {Array<number>} sample - The array of sampled periods.
+ * @property {number} sem - The standard error of the mean.
+ * @property {number} variance - The sample variance.
+ */
+
+/**
+ * run single function under benchmarking.
+ *
+ * @param {Function} fn - function to measure
+ * @returns {Promise<Stats>}
+ */
 function runSingle(fn) {
   return new Promise(function (resolve, reject) {
     var bench = new Benchmark({
@@ -43,6 +62,21 @@ function printResult(testContext, result) {
 }
 
 /**
+ * Get mean and median from Benchmark result.
+ *
+ * @param {Stats} stats - output stats of Benchmark#stats
+ * @param {number} stats.mean
+ * @param {Array<number>} stats.sample
+ * @returns {object} mean and median
+ */
+function getResultToPrint(stats) {
+  return {
+    mean: stats.mean,
+    median: median(stats.sample)
+  };
+}
+
+/**
  * Run a benchmark and print the results.
  *
  * @param {*} testContext - context object of mocha.
@@ -51,6 +85,7 @@ function printResult(testContext, result) {
  */
 function printBenchmark(testContext, fn) {
   return runSingle(fn)
+    .then(getResultToPrint)
     .then(function (result) {
       return printResult(testContext, result);
     });
